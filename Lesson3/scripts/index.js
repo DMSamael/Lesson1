d96dfd41751e2d0a0f7fa2cdcad3189d06250a0e
DMSamael <dmitriy.sulym@gmail.com>
2023-06-03 11:25:00 +0300

    Navigation

--- Lesson3/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const startBtn = document.getElementById('start');
-const output = document.getElementById('output');
-
-let count = 0;
-
-startBtn.addEventListener('click', () => {
-  count++;
-  output.textContent = count;
-});
